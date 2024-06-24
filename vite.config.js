9ae8cd5e41c98a80db14a93794b1434e7913272a
johurul-abir <mdjohurulislam555@gmail.com>
2024-06-25 00:21:14 +0600

    config update

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
     rollupOptions: {
-      input: "client/src/main.jsx", // Ensure the path is correct here
+      input: "client/src/main.jsx", // Ensure this path is correct
     },
+    outDir: "dist", // Default output directory
   },
 });
